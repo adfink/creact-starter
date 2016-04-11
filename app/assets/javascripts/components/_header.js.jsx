0b159c216f9149a8e8cb928eb7c25dbf45ea3be8
Andrew Fink <andrewfink8@gmail.com>
2016-04-11 15:25:23 -0700

    breaking into components

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,8 @@
+
+var Header = React.createClass({
+  render() {
+    return(
+      <h1>header stuff</h1>
+    )
+  }
+});
--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -5,7 +5,7 @@ var Main = React.createClass({
   render() {
     return(
       <div>
-        <h1>Hello, world that is connected to the internet, this is a rails app with a react front end</h1>
+        <Header />
       </div>
     )
   }
