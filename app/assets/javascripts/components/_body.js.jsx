5111b478f7c7f52f7fb1b9337e8dccefa5ee061d
Andrew Fink <andrewfink8@gmail.com>
2016-04-11 19:15:40 -0700

    posting new skill to the database and rerendering page with react immediately... first step of crud complete

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,11 +1,25 @@
 
 var Body = React.createClass({
+
+  handleSubmit(new_skill){
+    var newState = this.state.skills.concat(new_skill);
+    this.setState( { skills:newState } )
+  },
+
+  getInitialState(){
+    return { skills: [] }
+  },
+
+  componentDidMount(){
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
   render() {
     return(
       <div>
         <h1>body</h1>
-        <NewSkill />
-        <AllSkills /> 
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills} />
       </div>
     )
   }
--- app/assets/javascripts/components/_new_skill.js.jsx
+++ app/assets/javascripts/components/_new_skill.js.jsx
@@ -1,8 +1,32 @@
 
 var NewSkill = React.createClass({
+
+
+  handleClick(){
+    var name = this.refs.name.value;
+    var details = this.refs.details.value;
+
+    $.ajax({
+      url:'/api/v1/skills',
+      type: 'POST',
+      data: { skill: { name: name, details: details } },
+      success: (new_skill) => {
+        this.props.handleSubmit(new_skill);
+      }
+    });
+
+
+
+  },
+
   render() {
     return(
-      <h1>new skill</h1>
+      <div>
+        <input ref='name' placeholder='enter name of skill here' />
+        <input ref='details' placeholder='description of skill here' />
+        <button onClick={this.handleClick}>Submit</button>
+
+      </div>
     )
   }
 });
